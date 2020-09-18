39d8b01abe24310dfc2622adeb5a77aeb76695f7
Rajendran <swetha2@netapp.com>
2020-09-18 22:27:59 +0530

    changes in calculator.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -26,28 +26,4 @@ describe('Demo Calculator Test', function(){
 
 
     });
-    it('Subtraction Test', function(){
-
-        //browser.get('https://juliemr.github.io/protractor-demo/');
-        hp.get('https://juliemr.github.io/protractor-demo/');
-
-       //element(by.model('first')).sendKeys('5');
-        hp.enterfirstNumber('10');
-
-        //element(by.model('second')).sendKeys('10');
-        hp.entersecondNumber('15');
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-        hp.clickGo1();
-        
-        
-        //let result=element(by.cssContainingText('.ng-binding','15'));
-        //expect(result.getText()).toEqual('15');
-        
-        hp.verifyResults('25');
-
-        browser.sleep(2000);
-
-
-    });
 });
